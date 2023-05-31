f3ff953279630307e8bc0d715c20b328243881dc
Khoi <ngdangkhoi261002@gmail.com>
2023-06-01 00:17:53 +0700

    still have a bug

--- app/src/main/java/com/example/btl_nhom_7/MainActivity.java
+++ app/src/main/java/com/example/btl_nhom_7/MainActivity.java
@@ -9,14 +9,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
+    Button loginBtn,forgotPWBtn;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        loginBtn = findViewById(R.id.loginBtn);
+        //forgotPWBtn = findViewById(R.id.forgotPWBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(MainActivity.this , Home.class);
