13f5ea242313a9e553198bcc99d4c71293615620
Khoi <ngdangkhoi261002@gmail.com>
2023-06-01 11:05:12 +0700

    Home(Sign in) and forgot password design

--- app/src/main/java/com/example/btl_nhom_7/MainActivity.java
+++ app/src/main/java/com/example/btl_nhom_7/MainActivity.java
@@ -7,16 +7,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button loginBtn,forgotPWBtn;
+    TextView tvForgotPW;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loginBtn = findViewById(R.id.loginBtn);
-        //forgotPWBtn = findViewById(R.id.forgotPWBtn);
+        tvForgotPW = findViewById(R.id.tvForgotPW);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public void onForgotPasswordButtonClick(View v){
+        Intent intent = new Intent(MainActivity.this,ForgotPasswordActivity.class);
+        startActivity(intent);
+    }
+    public void onSignUpButtonClick(View v){
+        Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+        startActivity(intent);
     }
 }
