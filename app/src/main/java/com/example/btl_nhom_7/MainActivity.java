abe33ddb40f74a5a2377b3e9d8e90a948cb98fab
Khoi <ngdangkhoi261002@gmail.com>
2023-06-06 21:30:11 +0700

    Sign up design

--- app/src/main/java/com/example/btl_nhom_7/MainActivity.java
+++ app/src/main/java/com/example/btl_nhom_7/MainActivity.java
@@ -3,18 +3,26 @@ package com.example.btl_nhom_7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.btl_nhom_7.Motor.MotorActivity;
+import com.example.btl_nhom_7.User.database.UserDatabaseHelper;
+import com.example.btl_nhom_7.User.model.User;
 
 public class MainActivity extends AppCompatActivity {
     Button loginBtn,forgotPWBtn;
     TextView tvForgotPW;
+    EditText editPhoneNumber,editPassword;
+    Context context;
+    UserDatabaseHelper userDatabaseHelper;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +30,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         loginBtn = findViewById(R.id.loginBtn);
         tvForgotPW = findViewById(R.id.tvForgotPW);
+        editPassword = findViewById(R.id.editPassword);
+        editPhoneNumber = findViewById(R.id.editPhoneNumber);
+        editPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        userDatabaseHelper = new UserDatabaseHelper(this);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this , MotorActivity.class);
-                startActivity(intent);
+                String phoneNumber = editPhoneNumber.getText().toString();
+                String password = editPassword.getText().toString();
+                User user = userDatabaseHelper.getUser(phoneNumber);
+                if (user != null && user.getPassword().equals(password)) {
+                    Toast.makeText(getApplicationContext(),"Đăng nhập thành công!",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this,Home.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Sai tài khoản hoặc mật khẩu!",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
