fbf6d427d5d13945c8b39e5b54092b2fc6633bef
Long <long7102002@gmail.com>
2023-06-27 18:22:58 +0700

    update-home

--- app/src/main/java/com/example/btl_nhom_7/fragment/HomeFragment.java
+++ app/src/main/java/com/example/btl_nhom_7/fragment/HomeFragment.java
@@ -1,15 +1,9 @@
 package com.example.btl_nhom_7.fragment;
 
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
